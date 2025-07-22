d6c2a3e77cc524871882fac509f4f54cc14f44eb
sofia <sofiafamorim@gmail.com>
2025-07-22 00:18:04 -0300

    jogo da cobrinha primeiro commit

---
+++ src/cobrinha/snakeGame.java
@@ -0,0 +1,11 @@
+package cobrinha;
+
+public class snakeGame {
+
+	public static void main(String[] args) {
+		
+		new gameFrame();
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module cobrinha {
+	requires java.desktop;
+}
